5d095c9d2d01409d4a2727dc8fd806602bd2d9cb
Kunal <coolkunalgupta123@gmail.com>
2022-12-07 23:41:09 +0530

    created a form to add employee

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import EmployeeForm from './components/EmployeeForm';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar />
+      <EmployeeForm />
+    </>
   );
 }
 
